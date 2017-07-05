3f3060638cada996c13c83f3fd28360d477cb32d
Jo Giraerts <jo@rombit.be>
2017-07-05 02:11:30 +0200

    twitter consumer + queueing through beanstalk

--- Twitter/TwitterListener.php
+++ Twitter/TwitterListener.php
@@ -3,11 +3,21 @@ include_once "vendor/autoload.php";
 include_once "config.php";
 include_once "lib.php";
 
+use Pheanstalk\Pheanstalk;
+
 /**
  * Listen to my twitter-stream for certain posts
  */
 class TwitterListener extends OauthPhirehose
 {
+    var $client;
+
+    public function __construct($username, $password, $method = Phirehose::METHOD_SAMPLE, $format = self::FORMAT_JSON, $lang = FALSE)
+    {
+        parent::__construct($username, $password, $method, $format, $lang);
+        $this->client = new Pheanstalk('127.0.0.1');
+    }
+
     /**
      * Enqueue each status
      *
@@ -15,14 +25,13 @@ class TwitterListener extends OauthPhirehose
      */
     public function enqueueStatus($status)
     {
-        /*
-         * In this simple example, we will just display to STDOUT rather than enqueue.
-         * NOTE: You should NOT be processing tweets at this point in a real application, instead they should be being
-         *       enqueued and processed asyncronously from the collection process.
-         */
         $data = json_decode($status, true);
         if (is_array($data) && isset($data['user']['screen_name'])) {
             if (preg_match('/vicki.*bot/i', $data['user']['screen_name'])) {
+                // Put it on the queue
+                $this->client->putInTube('vickiqueue', $status);
+
+                // Make a log
                 print $data['user']['screen_name'] . ': ' . urldecode($data['text']) . "\n";
             }
         }
