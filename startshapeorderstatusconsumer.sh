87a986bbeb396e29523fce3a0d80fcb9f12b4a2f
Jo Giraerts <whachoe@gmail.com>
2017-07-21 23:21:23 +0200

    consumer start scripts

--- startshapeorderstatusconsumer.sh
+++ startshapeorderstatusconsumer.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/cjpa/shapeshift-api-cli
-php Shifters/Shapeshift/ShapeshiftOrderstatusConsumer.php >> /home/cjpa/shapeshift-api-cli/logs/consumer_orderstatus.log
+#php Shifters/Shapeshift/ShapeshiftOrderstatusConsumer.php >> /home/cjpa/shapeshift-api-cli/logs/consumer_orderstatus.log
+php Shifters/Changelly/ChangellyOrderstatusConsumer.php >> /home/cjpa/shapeshift-api-cli/logs/consumer_orderstatus.log
